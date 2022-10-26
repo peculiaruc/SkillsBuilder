907e4cec6a3fa2f90ca8444e3a634182df9412b4
raoularavel <rkuate1050@gmail.com>
2022-10-26 21:36:44 +0100

    Setting up  App theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material';
 import App from './App';
-import './index.css';
+import theme from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
