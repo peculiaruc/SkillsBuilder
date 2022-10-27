3cac9741e1630087c0f9c73318869fdb2c2a3580
raoularavel <rkuate1050@gmail.com>
2022-10-27 03:06:15 +0100

    Create ResetPasswordView

---
+++ frontend/src/pages/auth/ResetPasswordView.tsx
@@ -0,0 +1,47 @@
+import PasswordIcon from '@mui/icons-material/Password';
+import {
+  Button, InputAdornment, Stack, TextField
+} from '@mui/material';
+import Logo from '../../assets/images/Logo.png';
+
+export default function ResetPasswordView() {
+  return (
+    <Stack spacing={2}>
+      <Stack
+        spacing={1}
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img src={Logo} alt="Conversational App" />
+        <h2>Conversational App</h2>
+      </Stack>
+      <Stack spacing={2}>
+        <TextField
+          label="New Password"
+          type="password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <PasswordIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          label="Confirm Password"
+          type="password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <PasswordIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button>Submit</Button>
+      </Stack>
+    </Stack>
+  );
+}
