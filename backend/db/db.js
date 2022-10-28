7315d2f455ada621e04de7c520a4eb4e145e52b4
kwambokaB <paula.paulatek@gmail.com>
2022-10-28 22:32:34 +0300

    bug: codacy checks

--- backend/db/db.js
+++ backend/db/db.js
@@ -9,12 +9,12 @@ const pool = new Pool({
 
 export default {
   query: async (text, params) => {
-    try {
-      const res = await pool.query(text, params);
-      return res;
-    } catch (e) {
-      console.log(e);
-    }
+    // try {
+    const res = await pool.query(text, params);
+    return res;
+    // } catch (e) {
+    //   console.log(e);
+    // }
   },
   clearDb: async () => {
     await pool.query('DROP TABLE IF EXISTS ffff CASCADE');
