54437d14d85f70faf084ce02c0ccb10215cb7703
raoularavel <rkuate1050@gmail.com>
2022-10-30 20:42:30 +0100

    :bug: BUG: Bux fixed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ import db from '../db/db';
 dotenv.config();
 
 exports.createUser = async (req, res) => {
-  // try {
   const salt = await bcryptjs.genSalt(10);
   const hashedPassword = await bcryptjs.hash(req.body.password, salt);
 
@@ -38,17 +37,9 @@ exports.createUser = async (req, res) => {
       user: resp.rows[0],
     },
   }); 
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(400).json({
-  //     status: 'error',
-  //     error: err.message,
-  //   });
-  // }
 }
 
 exports.login = async (req, res) => {
-  // try {
   const user = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
 
   if (user.rows.length) {
@@ -76,11 +67,4 @@ exports.login = async (req, res) => {
     status: 'error',
     error: 'invalid email',
   });
-  // } catch (err) {
-  //   console.log(err);
-  //   return res.status(400).json({
-  //     status: 'error',
-  //     error: err.message,
-  //   });
-  // }
 }
