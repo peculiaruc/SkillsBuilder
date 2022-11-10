c8e5c5b1c76e1e343fc83bdd3fd0fcf8acbfb4a0
kwambokaB <paula.paulatek@gmail.com>
2022-11-10 10:58:13 +0300

    codacy check: fixing quality checks

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,133 +8,98 @@ import sendEmail from '../utils/sendEmails';
 dotenv.config();
 
 exports.createUser = async (req, res) => {
-  const salt = await bcryptjs.genSalt(10);
-  const hashedPassword = await bcryptjs.hash(req.body.password, salt);
-
-
-    const findUser = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
-    if (findUser.rows.length) {
+  try {
+    // check if user exist
+    const user = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
+    if (user.rows.length) {
       return res.status(400).json({
         status: 'error',
         error: 'User with email already exists. Please Log in',
       });
     }
 
-    const resp = await db.query(
+    // create user
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(req.body.password, salt);
+    const newUser = await db.query(
       'INSERT INTO users(fullName, email, password, city, auth_method) VALUES($1, $2, $3, $4, $5) RETURNING *',
       [req.body.fullname, req.body.email, hashedPassword, req.body.city, req.body.auth_method]
     );
 
-    //  create an auth token
-    const token = jwt.sign({ id: resp.rows[0].id }, process.env.JWT_PRIVATE_KEY, {
+    // send validation email
+    const randomToken = crypto.randomBytes(32).toString('hex');
+    const verifytoken = await db.query(
+      'INSERT INTO tokens(user_id, token) VALUES($1, $2) RETURNING *',
+      [newUser.rows[0].id, randomToken]
+    );
+    const link = `${process.env.BASE_URL}/api/v1/auth/verify-email/${newUser.rows[0].id}/${verifytoken.rows[0].token}`;
+    await sendEmail(newUser.rows[0].email, 'Verify Email', link);
+
+    // generate jwt token
+    const token = jwt.sign({ id: newUser.rows[0].id }, process.env.JWT_PRIVATE_KEY, {
       expiresIn: '24H',
     });
 
-    // send confirmation and verification Email
-    let randomToken = crypto.randomBytes(32).toString('hex');
-    let verifytoken = await db.query('SELECT * FROM tokens WHERE user_id = $1', [resp.rows[0].id]);
-    if (!verifytoken.rows.length) {
-      verifytoken = await db.query(
-        'INSERT INTO tokens(user_id, token) VALUES($1, $2) RETURNING *',
-        [resp.rows[0].id, randomToken]
-      );
-    } else {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Account not verified',
-      });
-    }
-
-    const link = `${process.env.BASE_URL}/api/v1/auth/verify-email/${resp.rows[0].id}/${verifytoken.rows[0].token}`;
-    // const body = ``
-    let sent = await sendEmail(resp.rows[0].email, 'Verify Email', link);
-
-    if (sent) {
-      return res.status(200).json({
-        status: 'success',
-        data: {
-          token,
-          user: resp.rows[0],
-        },
-      });
-    } else {
-      await db.query('DELETE FROM tokens WHERE user_id = $1', [resp.rows[0].id]);
-      return res.status(500).json({
-        status: 'error',
-        error: `Email not sent ${sent}`,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-
-    return res.status(400).json({
-      status: 'error',
-      error: 'User with email already exists. Please Log in',
-    });
-
-  }
-  
-  const resp = await db.query(
-    'INSERT INTO users(fullName, email, password, city, auth_method) VALUES($1, $2, $3, $4, $5) RETURNING *',
-    [req.body.fullname, req.body.email, hashedPassword, req.body.city, req.body.auth_method],
-  );
-
-  console.log(resp.rows[0]);
-
-  //  create token
-  const token = jwt.sign({ id: resp.rows[0].id }, process.env.JWT_PRIVATE_KEY, {
-    expiresIn: '24H',
-  });
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      token,
-      user: resp.rows[0],
-    },
-  });
-};
-
-exports.login = async (req, res) => {
-  const user = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
-
-  if (user.rows.length) {
-    const validPass = await bcryptjs.compare(req.body.password, user.rows[0].password);
-
-    if (!validPass) {
-      return res.status(400).json({
-        status: 'error',
-        error: 'invalid Password',
-      });
-    }
-    //  create token
-    const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_PRIVATE_KEY, {
-      expiresIn: '24H',
-    });
     return res.status(200).json({
       status: 'success',
       data: {
         token,
-        user: user.rows[0],
+        user: newUser.rows[0],
       },
     });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: 'error',
+      error: err.message,
+    });
   }
-  return res.status(400).json({
-    status: 'error',
-    error: 'invalid email',
-  });
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
+
+    if (user.rows.length) {
+      const validPass = await bcryptjs.compare(req.body.password, user.rows[0].password);
+
+      if (!validPass) {
+        return res.status(400).json({
+          status: 'error',
+          error: 'invalid Password',
+        });
+      }
+      //  create token
+      const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_PRIVATE_KEY, {
+        expiresIn: '24H',
+      });
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          token,
+          user: user.rows[0],
+        },
+      });
+    }
+    return res.status(400).json({
+      status: 'error',
+      error: 'invalid email',
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: 'error',
+      error: err.message,
+    });
+  }
 };
 
 exports.passwordReset = async (req, res) => {
   try {
-    // const schema = Joi.object({ email: Joi.string().email().required() });
-    // const { error } = schema.validate(req.body.email);
-    // if (error) return res.status(400).send(error.details[0].message);
-
     // check if email exists
-    const findUser = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
+    const user = await db.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
 
-    if (!findUser.rows.length) {
+    if (!user.rows.length) {
       return res.status(400).json({
         status: 'error',
         error: 'User with given email does not exist',
@@ -142,7 +107,7 @@ exports.passwordReset = async (req, res) => {
     }
 
     // create token if dosent exist
-    let userId = findUser.rows[0].id;
+    let userId = user.rows[0].id;
     let randomToken = crypto.randomBytes(32).toString('hex');
     let token = await db.query('SELECT * FROM tokens WHERE user_id = $1', [userId]);
     if (!token.rows.length) {
@@ -159,7 +124,7 @@ exports.passwordReset = async (req, res) => {
 
     // email user the link
     const link = `${process.env.BASE_URL}/password-reset/${userId}/${token.rows[0].token}`;
-    let sent = await sendEmail(req.body.email, 'Password reset', link);
+    const sent = await sendEmail(req.body.email, 'Password reset', link);
 
     if (sent) {
       return res.status(200).json({
