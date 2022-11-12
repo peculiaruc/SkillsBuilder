a70daebb3385a0e40422e2d3a19aa2a08fb40e5d
raoularavel <rkuate1050@gmail.com>
2022-11-12 17:04:43 +0100

    Code quality check

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import userController from '../controllers/userController';
 
-
 const router = express.Router();
 
 router.post('/register', userController.createUser);
