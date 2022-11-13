0f56120b5d5e3ae38cffb85d2a2cf8ee4a9b1431
raoularavel <rkuate1050@gmail.com>
2022-11-13 21:14:17 +0100

    :rotating_light: Bug: Linter warnings fixed.

--- backend/utils/tokentools.js
+++ backend/utils/tokentools.js
@@ -2,8 +2,7 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 
 module.exports = {
-  createUserToken: (user) =>
-    jwt.sign({ id: user.id }, process.env.JWT_PRIVATE_KEY, {
+  createUserToken: (user) => jwt.sign({ id: user.id }, process.env.JWT_PRIVATE_KEY, {
       expiresIn: '24H',
     }),
   createRandomToken: () => crypto.randomBytes(32).toString('hex'),
