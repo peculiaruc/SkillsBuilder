4cfe16dda94497cbc67ec2e7c6e0bc1a166954da
kwambokaB <paula.paulatek@gmail.com>
2022-11-14 21:05:27 +0300

    fix: unnecessary block

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -1,136 +1,138 @@
 import db from '../db/db';
 import sendEmail from '../utils/sendEmails';
 
-exports.getAllCourses = async (req, res) => {
-  const { offset, limit } = req.query;
-  try {
-    const total = await db.query('SELECT COUNT(*) FROM courses');
-    const course_data = await db.query('SELECT * FROM courses LIMIT $2 OFFSET $1', [
-      offset || 0,
-      limit || 5,
-    ]);
+module.exports = {
+  getAllCourses: async (req, res) => {
+    const { offset, limit } = req.query;
+    try {
+      const total = await db.query('SELECT COUNT(*) FROM courses');
+      const course_data = await db.query('SELECT * FROM courses LIMIT $2 OFFSET $1', [
+        offset || 0,
+        limit || 5,
+      ]);
 
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        totalCourses: total.rows[0].count,
-        courses: course_data.rows,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
-};
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          totalCourses: total.rows[0].count,
+          courses: course_data.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 
-exports.getCoursesByCategories = async (req, res) => {
-  const { categories } = req.body;
-  try {
-    const course_data = await db.query('SELECT * FROM  courses WHERE category_ids @> $1', [
-      categories,
-    ]);
+  getCoursesByCategories: async (req, res) => {
+    const { categories } = req.body;
+    try {
+      const course_data = await db.query('SELECT * FROM  courses WHERE category_ids @> $1', [
+        categories,
+      ]);
 
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        totalCourses: course_data.rowCount,
-        courses: course_data.rows,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
-};
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          totalCourses: course_data.rowCount,
+          courses: course_data.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 
-exports.getCoursesById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const course_data = await db.query('SELECT * FROM  courses WHERE id = $1', [id]);
+  getCoursesById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const course_data = await db.query('SELECT * FROM  courses WHERE id = $1', [id]);
 
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        course: course_data.rows,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
-};
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          course: course_data.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 
-exports.getEnrolledCourses = async (req, res) => {
-  const { user_id } = req.body;
-  try {
-    const enrolled_courses = await db.query('SELECT * FROM enrollments WHERE user_id = $1', [
-      user_id,
-    ]);
+  getEnrolledCourses: async (req, res) => {
+    const { user_id } = req.body;
+    try {
+      const enrolled_courses = await db.query('SELECT * FROM enrollments WHERE user_id = $1', [
+        user_id,
+      ]);
 
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        totalCourses: enrolled_courses.rowCount,
-        courses: enrolled_courses.rows,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
-};
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          totalCourses: enrolled_courses.rowCount,
+          courses: enrolled_courses.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 
-exports.getCourseCategory = async (req, res) => {
-  try {
-    const categories_data = await db.query('SELECT * FROM course_categories');
-    return res.status(200).json({
-      status: 'success',
-      data: {
-        categories: categories_data.rows,
-      },
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
-};
+  getCourseCategory: async (req, res) => {
+    try {
+      const categories_data = await db.query('SELECT * FROM course_categories');
+      return res.status(200).json({
+        status: 'success',
+        data: {
+          categories: categories_data.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 
-exports.enrollUser = async (req, res) => {
-  const { user_id, course_id, course_name } = req.body;
-  try {
-    const user = await db.query('SELECT * FROM users WHERE id = $1', [user_id]);
-    await db.query(
-      'INSERT INTO enrollments(user_id, course_id, enroll_date) VALUES($1, $2, $3) RETURNING *',
-      [user_id, course_id, new Date()]
-    );
-    await sendEmail(
-      user.rows[0].email,
-      'Enrollment Confirmation',
-      `You have successfully enrolled in ${course_name}`
-    );
-    return res.status(200).json({
-      status: 'success',
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({
-      status: 'error',
-      error: err.message,
-    });
-  }
+  enrollUser: async (req, res) => {
+    const { user_id, course_id, course_name } = req.body;
+    try {
+      const user = await db.query('SELECT * FROM users WHERE id = $1', [user_id]);
+      await db.query(
+        'INSERT INTO enrollments(user_id, course_id, enroll_date) VALUES($1, $2, $3) RETURNING *',
+        [user_id, course_id, new Date()]
+      );
+      await sendEmail(
+        user.rows[0].email,
+        'Enrollment Confirmation',
+        `You have successfully enrolled in ${course_name}`
+      );
+      return res.status(200).json({
+        status: 'success',
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        status: 'error',
+        error: err.message,
+      });
+    }
+  },
 };
