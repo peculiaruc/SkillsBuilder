cb7e75784caab02fa53302c2f1d48aaaaf07781d
raoularavel <rkuate1050@gmail.com>
2022-11-19 20:42:07 +0100

    FEAT: Create a Basic Form Builder Component

---
+++ frontend/src/forms/FormBuilder.tsx
@@ -0,0 +1,38 @@
+import { Stack } from '@mui/material';
+import { FormikValues, useFormik } from 'formik';
+import Model from '../../models/Model';
+import MixedInput from './inputs/MixedInput';
+
+interface FormProps {
+  model: Model,
+  onSubmit: (values: FormikValues) => void,
+  onClose: () => void,
+  onOpen: () => void,
+}
+
+function FormBuilder(props: FormProps) {
+  const { onSubmit, model } = props;
+  const formik = useFormik({
+    initialValues: model.initialValues,
+    validationSchema: model.validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <Stack spacing={2}>
+        {model.fields.map(
+          (field) => (
+            <MixedInput
+              {...field}
+              onChange={formik.handleChange}
+              key={field.name}
+            />
+          ),
+        )}
+      </Stack>
+    </form>
+  );
+}
+
+export default FormBuilder;
