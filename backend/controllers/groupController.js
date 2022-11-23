52c069c4a131dc5a5d10ac8d5451964ddf9c581f
kwambokaB <paula.paulatek@gmail.com>
2022-11-23 11:58:41 +0300

    codacy

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -6,20 +6,18 @@ const group = new Group();
 const joinedG = new JoinedGroup();
 
 class GroupController {
-  // create group
   static async createGroup(req, res) {
-    const { name, userId, courseId } = req.body;
     const newGroup = {
-      name,
-      creator_id: userId,
-      course_id: courseId,
+      name: req.body.name,
+      creator_id: req.body.userId,
+      course_id: req.body.courseId,
     };
     const _group = await group.create(newGroup);
     if (_group.errors) {
       return Helpers.dbError(res, _group);
     }
     const newJoin = {
-      user_id: userId,
+      user_id: req.body.userId,
       group_id: _group.rows[0].id,
       join_date: new Date(),
     };
@@ -30,12 +28,10 @@ class GroupController {
     return Helpers.sendResponse(res, 200, 'Group created successfully', { group: _group.rows[0] });
   }
 
-  // join group
   static async joinGroup(req, res) {
-    const { groupId, userId } = req.body;
     const newJoin = {
-      user_id: userId,
-      group_id: groupId,
+      user_id: req.body.userId,
+      group_id: req.body.groupId,
       join_date: new Date(),
     };
     const _join = await joinedG.create(newJoin);
@@ -47,21 +43,17 @@ class GroupController {
     });
   }
 
-  // delete group
   static async deleteGroup(req, res) {
-    const { id } = req.params;
-    const _group = await group.delete({ id });
+    const _group = await group.delete({ id: req.params.id });
     if (_group.errors) {
       return Helpers.dbError(res, _group);
     }
     return Helpers.sendResponse(res, 200, 'Group deleted successfully');
   }
 
-  // leave group
   static async leaveGroup(req, res) {
-    const { groupId } = req.body;
     const data = { leave_date: new Date() };
-    const where = { id: groupId };
+    const where = { id: req.body.groupId };
     const _group = await joinedG.update(data, where);
     if (_group.errors) {
       return Helpers.dbError(res, _group);
@@ -69,10 +61,8 @@ class GroupController {
     return Helpers.sendResponse(res, 200, 'Group left successfully');
   }
 
-  // get users joined groups
   static async myGroups(req, res) {
-    const { userId } = req.body;
-    const _group = await joinedG.allWhere({ user_id: userId });
+    const _group = await joinedG.allWhere({ user_id: req.body.userId });
     if (_group.errors) {
       return Helpers.dbError(res, _group);
     }
@@ -80,27 +70,21 @@ class GroupController {
     return Helpers.sendResponse(res, 200, 'success', { myGroups: _group.rows });
   }
 
-  // get one group
   static async groupById(req, res) {
-    const { id } = req.params;
-    const _group = await group.getById(id);
+    const _group = await group.getById(req.params.id);
     if (_group.errors) {
       return Helpers.dbError(res, _group);
     }
     return Helpers.sendResponse(res, 200, 'success', { groups: _group.row });
   }
 
-  // get group by course
   static async groupByCourse(req, res) {
-    const { courseId } = req.params;
-    const _group = await group.getByCourse(courseId);
+    const _group = await group.getByCourse(req.params.courseId);
     if (_group.errors) {
       return Helpers.dbError(res, _group);
     }
-    return Helpers.sendResponse(res, 200, 'success', { groups: _group.row});
+    return Helpers.sendResponse(res, 200, 'success', { groups: _group.row });
   }
-
-  //
 }
 
 export default GroupController;
