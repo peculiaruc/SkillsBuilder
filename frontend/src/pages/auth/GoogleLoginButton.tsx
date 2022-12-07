6dd54194e93e468b7d376f6314b66211e909b3ac
raoularavel <rkuate1050@gmail.com>
2022-12-07 03:01:54 +0100

    Auhentication - Add spinner on GoogleButton and LinkedIn button

--- frontend/src/pages/auth/GoogleLoginButton.tsx
+++ frontend/src/pages/auth/GoogleLoginButton.tsx
@@ -1,10 +1,14 @@
 import { Google } from '@mui/icons-material';
 import { useGoogleLogin } from '@react-oauth/google';
+import { ThreeDots } from 'react-loader-spinner';
 import { nanoid } from 'nanoid';
 import { SocialLoginRequest, useGoogleLoginMutation } from '../../apiServices/authService';
+import { usePalette } from '../../theme/theme';
 
 function GoogleLoginButton() {
-  const [authWithGoogle] = useGoogleLoginMutation();
+  const palette = usePalette();
+
+  const [authWithGoogle, { isLoading }] = useGoogleLoginMutation();
 
   const onSuccess = async (data: Partial<SocialLoginRequest>) => {
     await authWithGoogle({
@@ -19,7 +23,16 @@ function GoogleLoginButton() {
     flow: 'auth-code',
   });
 
-  return <Google fontSize="large" onClick={() => GoogleLogin()} />;
+  return !isLoading ? <Google fontSize="large" onClick={() => GoogleLogin()} /> : (
+    <ThreeDots
+      height="20"
+      width="100%"
+      radius="9"
+      color={palette.primary.main}
+      ariaLabel="three-dots-loading"
+      visible
+    />
+  );
 }
 
 export default GoogleLoginButton;
