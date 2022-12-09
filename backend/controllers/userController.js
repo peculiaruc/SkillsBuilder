e50dbfc4429baaf0753db6c5772b25782a5a6a79
kwambokaB <paula.paulatek@gmail.com>
2022-12-09 16:42:18 +0300

    feat: get author assignment endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -189,20 +189,20 @@ class UserController {
 
   static async getUserPosts(req, res) {
     const _posts = await post.allWhere({ owner_id: req.params.id });
-    if (_posts.errror) return Helpers.dbError(res, _posts);
+    if (_posts.errors) return Helpers.dbError(res, _posts);
     return Helpers.sendResponse(res, 200, SUCCESS, { posts: _posts.rows });
   }
 
   static async getAuthorAssignments(req, res) {
     const _assignment = await assignment.allWhere({ author_id: req.params.id });
-    if (_assignment.errror) return Helpers.dbError(res, _assignment);
+    if (_assignment.errors) return Helpers.dbError(res, _assignment);
     return Helpers.sendResponse(res, 200, SUCCESS, { assignments: _assignment.rows });
   }
 
   static async getAuthorsCourses(req, res) {
     const _courses = await course.allWhere({ author_id: req.params.id });
     console.log('author courses', _courses);
-    if (_courses.errror) return Helpers.dbError(res, _courses);
+    if (_courses.errors) return Helpers.dbError(res, _courses);
     return Helpers.sendResponse(res, 200, SUCCESS, { courses: _courses.rows });
   }
 }
