d91b582cb192379d892eb078a6ef72d4442482b9
kwambokaB <paula.paulatek@gmail.com>
2022-12-15 16:17:56 +0300

    fix: bot error

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('short'));
-app.use(botInit());
 
 app.use('/home', (req, res) => {
   res.status(200).send('Welcome to this awesome API!!');
@@ -56,7 +55,7 @@ app.use((req, res) => {
 
 app.listen(PORT, async () => {
   await initializeDb();
-  // await botInit();
+  botInit();
   console.log(`Listening on port: ${PORT}`);
 });
 
