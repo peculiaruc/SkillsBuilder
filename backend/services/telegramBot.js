bf3dca67825533ae17ad9c559795079ad2f505ce
kwambokaB <paula.paulatek@gmail.com>
2022-12-15 17:49:15 +0300

    fix: eslint

--- backend/services/telegramBot.js
+++ backend/services/telegramBot.js
@@ -9,40 +9,40 @@ dotenv.config();
 export const bot = new Telegraf(process.env.BOT_TOKEN);
 
 export default async () => {
-  try {
-    bot.start((ctx) => {
-      // console.log('srtart', ctx);
-      ctx.reply('Welcome to SkillBuddy Bot! Your learning partner. To get started type in /hello');
-    });
+  //  try {
+  bot.start((ctx) => {
+    // console.log('srtart', ctx);
+    ctx.reply('Welcome to SkillBuddy Bot! Your learning partner. To get started type in /hello');
+  });
 
-    bot.hears('/hello', (ctx) => {
-      console.log('hello', ctx);
-      ctx.reply('Hey there. if this is the first time please reply with your skillBuddy email');
-    });
+  bot.hears('/hello', (ctx) => {
+    console.log('hello', ctx);
+    ctx.reply('Hey there. if this is the first time please reply with your skillBuddy email');
+  });
 
-    bot.email(async (ctx) => {
-      console.log(ctx.update.message);
-      const data = {
-        email: ctx.update.message.text,
-        chat_id: ctx.update.message.chat.id,
-      };
-      const text = await TelegramController.updateUser(data);
-      console.log('text', text);
-      ctx.reply(text);
-    });
+  bot.email(async (ctx) => {
+    console.log(ctx.update.message);
+    const data = {
+      email: ctx.update.message.text,
+      chat_id: ctx.update.message.chat.id,
+    };
+    const text = await TelegramController.updateUser(data);
+    console.log('text', text);
+    ctx.reply(text);
+  });
 
-    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+  bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-    bot.catch((err, ctx) => {
-      console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
-    });
+  bot.catch((err, ctx) => {
+    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
+  });
 
-    bot.launch();
+  bot.launch();
 
-    // Enable graceful stopnumber
-    process.once('SIGINT', () => bot.stop('SIGINT'));
-    process.once('SIGTERM', () => bot.stop('SIGTERM'));
-  } catch (e) {
-    console.log('bot error', e);
-  }
+  // Enable graceful stopnumber
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+//   } catch (e) {
+//     console.log('bot error', e);
+  //  }
 };
